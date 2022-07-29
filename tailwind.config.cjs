2322df502f4614dafda2b4401bb62f8a304b0aaa
laura <lauras_here@pm.me>
2022-07-29 12:14:12 -0700

    Update colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#011627",
+        light: "#FDFFFC",
+        "accent-blue": "#2EC4B6",
+        "accent-red": "#E71D36",
+        theme: "#FF9F1C",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
